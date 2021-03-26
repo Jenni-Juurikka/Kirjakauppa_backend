c8c06e06f3280e975ddd60a4b88379fc566837e4
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-03-26 10:36:10 +0200

    muokattu ja pari uutta tiedostoa

--- products/getcategories.php
+++ products/getcategories.php
@@ -2,9 +2,11 @@
 require_once '../inc/functions.php';
 require_once '../inc/headers.php';
 
+print("<p>toimii</p>");
+
 try {
     $db = openDb();
-    selectAsJson($db, 'select * from category');
+    selectAsJson($db, 'select * from tuoteryhma');
 }
 catch (PDOException $pdoex) {
     returnError($pdoex);
