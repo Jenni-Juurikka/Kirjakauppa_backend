6b778cd7863baf04b08b4c236037f4e00e29085d
Joanna Turunen <n0tujo00@students.oamk.fi>
2021-03-26 11:27:33 +0200

    getproducts tehty

---
+++ products/getproducts.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$uri = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+
+$parameters = explode('/',$uri);
+
+$trnro = $uri[1];
+
+try {
+    $db = openDb();
+    selectAsJson($db,"select * from tuote where trnro = $trnro");
+
+}
+catch (PDOException $doexp) {
+    returnError($pdoex);
+}
