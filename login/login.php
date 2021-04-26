ab20ee33546dd04ecd3cde26ab79da72531c75a0
unknown <n0majo00@students.oamk.fi>
2021-04-26 10:22:28 +0300

    loginille oma kansio

--- login/login.php
+++ login/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once 'inc/headers.php';
-require_once 'inc/functions.php';
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
 
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'salasana', FILTER_SANITIZE_STRING);
