70b33a7345c38ccfa03bea2a18dfdb9c9ee196e1
Jenni Juurikka <c0juje01@students.oamk.fi>
2021-05-03 00:23:19 +0300

    Sisennys

--- products/getcategories.php
+++ products/getcategories.php
@@ -1,11 +1,11 @@
 <?php
-require_once '../inc/functions.php';
-require_once '../inc/headers.php';
+    require_once '../inc/functions.php';
+    require_once '../inc/headers.php';
 
-try {
-    $db = openDb();
-    selectAsJson($db, 'select * from tuoteryhma');
-}
-catch (PDOException $pdoex) {
-    returnError($pdoex);
-}
+    try {
+        $db = openDb();
+        selectAsJson($db, 'select * from tuoteryhma');
+    }
+    catch (PDOException $pdoex) {
+        returnError($pdoex);
+    }
